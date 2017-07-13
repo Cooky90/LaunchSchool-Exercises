bbb68e323f13d94240a9d1fdde23ceebf4e4e12e
Cooky90 <ashley.m.d.cook@gmail.com>
2017-07-13 11:19:30 +0100

    Completed all the variable exercises.

---
+++ Variables Exercise/name.rb
@@ -0,0 +1,16 @@
+puts "Please enter your name:"
+name = gets.chomp
+puts "Hello, #{name}"
+
+####New Section###
+10.times do
+  puts name
+end
+
+###New Section###
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your second name:"
+second_name = gets.chomp
+
+puts "Your name is #{first_name} #{second_name}"
