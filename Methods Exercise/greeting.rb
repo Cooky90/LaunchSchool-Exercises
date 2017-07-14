d5a8b2f342602c1bb606c507568f85539307249e
Cooky90 <ashley.m.d.cook@gmail.com>
2017-07-14 16:49:43 +0100

    Completed new exercises

---
+++ Methods Exercise/greeting.rb
@@ -0,0 +1,14 @@
+#greeting exercise
+
+def greeting(name)
+  "Hello, " + name
+end
+
+puts greeting("Ash")
+
+
+### x = 2 will evaluate to '2'
+### puts x = 2 will evaluate to nil
+### p name - Joe will evaluate to Joe
+### four = "four" will evaluate to four
+### print something = nothing will return nil
---
+++ Methods Exercise/multiply.rb
@@ -0,0 +1,16 @@
+def multiply(number1,number2)
+  number1 * number2
+end
+
+puts multiply(5,5)
+
+
+### Exercise 5, make this the method return the correct information and then put it to the screen.
+def scream(words)
+  words = words + "!!!!"
+  return words
+end
+
+puts scream("Yippeee")
+
+#Exercise 6, it returns the message because the wrong number of arguments are being passed to the method. The parameters would need to be looked at in order to check what should be taken.
